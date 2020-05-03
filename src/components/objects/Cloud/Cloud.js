8fb2ac46fa4261b7ef90535f73a4e1e9ce1dd16e
Lauren Johnston <laurenjohnston97@gmail.com>
2020-05-03 14:20:59 -0700

    Update Cloud.js
    Basic cloud geom merging

--- src/components/objects/Cloud/Cloud.js
+++ src/components/objects/Cloud/Cloud.js
@@ -1,14 +1,31 @@
-import { Group, IcosahedronGeometry, MeshBasicMaterial, Mesh} from 'three';
+import { Group, Geometry, IcosahedronGeometry, MeshStandardMaterial, Mesh} from 'three';
 
 class Cloud extends Group {
     constructor() {
         // Call parent Group() constructor
         super();
 
-        let radius = 5;
+        let radius = 1;
         let detail = 1;
-        var geometry = new IcosahedronGeometry(radius, detail);
-        var material = new MeshBasicMaterial( { color: 0x00ff00 } );
+
+        // Inspired by this to combine shapes to make clouds: https://medium.com/@joshmarinacci/procedural-geometry-low-poly-clouds-b86a0e66bcad
+        var geometry = new Geometry();
+
+        // create three sections of cloud
+        var section1 = new IcosahedronGeometry(radius, detail);
+        var section2 = new IcosahedronGeometry(radius, detail);
+        var section3 = new IcosahedronGeometry(radius, detail);
+
+        // translate sections
+        section1.translate(-1,0,0);
+        section3.translate(1, 0,0);
+
+        geometry.merge(section1);
+        geometry.merge(section2);
+        geometry.merge(section3);
+
+        var material = new MeshStandardMaterial( { color: 0xffffff, transparent: true, opacity: 0.5} );
+        //material.opacity = 0.3;
         var cloud = new Mesh( geometry, material );
         this.add(cloud);
     }
