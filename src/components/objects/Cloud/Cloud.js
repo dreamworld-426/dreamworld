b29ecbd776d3c893666bfc4130ca961c241e35d9
Lauren Johnston <laurenjohnston97@gmail.com>
2020-05-04 19:51:31 -0700

    Basic cloud
    Basic single cloud in scene

--- src/components/objects/Cloud/Cloud.js
+++ src/components/objects/Cloud/Cloud.js
@@ -1,11 +1,12 @@
-import { Group, Geometry, IcosahedronGeometry, MeshStandardMaterial, Mesh} from 'three';
+import { Group, Color, TextureLoader, IcosahedronGeometry, PlaneBufferGeometry, Geometry, MeshBasicMaterial, MeshLambertMaterial, ShaderMaterial, Mesh, FrontSide} from 'three';
+import img from './cloudy3.png';
 
 class Cloud extends Group {
     constructor() {
         // Call parent Group() constructor
         super();
 
-        let radius = 1;
+        let radius = 0.5;
         let detail = 1;
 
         // Inspired by this to combine shapes to make clouds: https://medium.com/@joshmarinacci/procedural-geometry-low-poly-clouds-b86a0e66bcad
@@ -13,21 +14,56 @@ class Cloud extends Group {
 
         // create three sections of cloud
         var section1 = new IcosahedronGeometry(radius, detail);
-        var section2 = new IcosahedronGeometry(radius, detail);
-        var section3 = new IcosahedronGeometry(radius, detail);
+        var section2 = new IcosahedronGeometry(0.4, detail);
+        var section3 = new IcosahedronGeometry(0.3, detail);
+        var section4 = new IcosahedronGeometry(0.25, detail);
 
         // translate sections
-        section1.translate(-1,0,0);
-        section3.translate(1, 0,0);
+        section2.translate(0,0,-2);
+        section1.translate(-0.5,0,-2);
+        section3.translate(0.5, 0,-2);
+        section4.translate(-0.8, -0.25,-2);
 
+
+        // merge 4 cloud parts together
         geometry.merge(section1);
         geometry.merge(section2);
         geometry.merge(section3);
+        geometry.merge(section4);
 
-        var material = new MeshStandardMaterial( { color: 0xffffff, transparent: true, opacity: 0.5} );
-        //material.opacity = 0.3;
+
+        let uniforms = {
+            colorB: {type: 'vec3', value: new Color(0xb4c1c6)},
+            colorA: {type: 'vec3', value: new Color(0xe1f1f7)}
+        }
+
+        // Jitter function (used only for testing): https://medium.com/@joshmarinacci/procedural-geometry-low-poly-clouds-b86a0e66bcad
+        // remap value from the range of [smin,smax] to [emin,emax]
+        const map = (val, smin, smax, emin, emax) => (emax-emin)*(val-smin)/(smax-smin) + emin;
+        // randomly displace the x,y,z coords by the `per` value
+        const jitter = (geometry,per) => geometry.vertices.forEach(v => {
+            v.x += map(Math.random(),0,1,-per,per);
+            v.y += map(Math.random(),0,1,-per,per);
+            v.z += map(Math.random(),0,1,-per,per);
+        })
+        //jitter(geometry,0.1);
+
+
+        var texture = new TextureLoader().load(img);
+
+        let material = new MeshLambertMaterial({
+            map:texture,
+            transparent: true,
+            opacity: 0.95,
+            side: FrontSide,
+          });
+
+          
+
         var cloud = new Mesh( geometry, material );
+
         this.add(cloud);
+
     }
 }
 
