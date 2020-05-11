87ce17cd7344cf2070d97a52e124ee111eae523b
Lydia You <lydia.g.you@gmail.com>
2020-05-11 16:30:36 -0400

    Fixed sun and added aura (goes east to west now around the bird, and is loaded after the terrain - doesn't go through terrain)), added sky textures, added more audio files, added some more small lighting adjustments.

--- src/components/lights/BasicLights.js
+++ src/components/lights/BasicLights.js
@@ -1,4 +1,4 @@
-import { Group, SpotLight, AmbientLight, HemisphereLight } from 'three';
+import { Group, SpotLight, AmbientLight, HemisphereLight, DirectionalLightHelper, DirectionalLight } from 'three';
 
 class BasicLights extends Group {
     constructor(...args) {
@@ -8,15 +8,27 @@ class BasicLights extends Group {
         const dir = new SpotLight(0xffffff, 1.6, 7, 0.8, 1, 1);
         const ambi = new AmbientLight(0x404040, 0.5); // overall lighting of the scene, doesn't cast shadows
         // const hemi = new HemisphereLight(0xffffbb, 0x080820, 1.0); // majority of the light (skycolor, groundcolor, intesnity)
-        const hemi = new HemisphereLight(0xcce0ff, 0x080820, 1.0);
+        const hemi = new HemisphereLight(0xcce0ff, 0x080820, 0.5);
         hemi.power = 800;
         hemi.decay = 2;
+
+        // Add light right above bird
+        // const birdLight = new (0xffffff, 10, 20, Math.PI/2, 0, 2);
+        var birdLight = new DirectionalLight(0xffffff, 3);
+        birdLight.power = 800; // 4 * Math.PI
+        birdLight.position.set(0, 50, -100);
+        birdLight.target.position.set(0, 0, 0);
+        birdLight.castShadow = true;
+        birdLight.decay = 2;
+        birdLight.distance = Infinity;
+
+        var helper = new DirectionalLightHelper(birdLight);
 
         // SpotLight needs a position and target
         dir.position.set(5, 1, 2);
         dir.target.position.set(0, 0, 0);
 
-        this.add(dir, ambi, hemi);
+        this.add(dir, ambi, hemi, birdLight, helper);
     }
 }
 
--- src/components/lights/WorldLighting.js
+++ src/components/lights/WorldLighting.js
@@ -1,7 +1,5 @@
-import { Group, DirectionalLight, SpotLight, SpotLightHelper, SphereBufferGeometry, MeshStandardMaterial, PointLight, Mesh, DirectionalLightHelper } from 'three';
+import { Group, DirectionalLight, SpotLight, SpotLightHelper, SphereBufferGeometry, MeshStandardMaterial, PointLight, Mesh, DirectionalLightHelper, Vector3 } from 'three';
 var ColorTween = require('color-tween');
-
-// const COLORTWEEN = require('color-tween/src/tween.js');
 
 class WorldLighting extends Group {
 
@@ -11,60 +9,68 @@ class WorldLighting extends Group {
 
       // Init state
       this.state = {
-          gui: parent.state.gui,
-          zrotation: 0.0,
-      };
+        sun: null
+      }
 
       // add sun
-      var sunGeometry = new SphereBufferGeometry(10, 25, 25);
-
-      // var sun = new PointLight(0xffee88, 100, 20, 200);
+      var sunGeometry = new SphereBufferGeometry(20, 25, 25);
       var sun = new DirectionalLight(0xffee88, 7);
       var sunMat = new MeshStandardMaterial({
 					emissive: 0xffffee,
 					emissiveIntensity: 2,
-					color: 0xffee88
-			} );
+					color: 0xffee88,
+          transparent: true,
+          depthOrder: 1
+			});
       sun.add(new Mesh(sunGeometry, sunMat));
       sun.target.position.set(0, 0, 0);
-      sun.position.set(0, 100, 500);
+      sun.position.set(0, 100, 700);
 			sun.castShadow = true;
       sun.power = 800;
       sun.decay = 2;
       sun.distance = Infinity;
+      this.state.sun = sun;
 
-      // Add light right above bird
-      var birdLight = new SpotLight(0xffffff, 10, 20, Math.PI/2, 0, 2);
-      // var birdLight = new DirectionalLight(0xffee88, 7);
-      birdLight.power = 4*Math.PI;
-      birdLight.position.set(0, 50, 0);
-      birdLight.target.position.set(0, 0, 0);
-      birdLight.castShadow = true;
+      // glow aura around sun
+      var auraSphere = new SphereBufferGeometry(35, 25, 25);
+      var aura = new DirectionalLight(0xffee88, 0.5);
+      var glowMat = new MeshStandardMaterial({
+					emissive: 0xffffee,
+					emissiveIntensity: 1,
+					color: 0xffee88,
+          transparent: true,
+          wireframe: false,
+          opacity: 0.35
+			} );
+      aura.add(new Mesh(auraSphere, glowMat));
+      aura.target.position.set(0, 0, 0);
+      aura.position.set(0, 100, 720);
 
       // add visualization helper for debugging
       const helper = new DirectionalLightHelper(sun);
-      // const birdHelper = new SpotLightHelper(birdLight);
+
+      var blue = new PointLight(0x0000ff, 1, 20, 2);
+      // blue.target.position.set(0, 50, 50);
+      var pink = new DirectionalLight(0xFFB6C1, 1, 50, 2);
+      // pink.target.position.set(0, 50, 50);
 
       this.add(helper);
       this.add(sun);
-      this.add(birdLight);
+      this.add(aura);
+      this.add(blue);
+      this.add(pink);
 
       // add this object to SeedScene's update list
       parent.addToUpdateList(this);
-      this.state.gui.add(this.state, 'zrotation', 0, 180);
-
-      // var tween = new ColorTween('#000', '#FFF')
-      //             .onUpdate(someFn)
-      //             .easing('linear')
-      //             .duration(10000)
-      //             .start(animate);
     }
 
+    // move the sun
     update(timeStamp) {
-      this.rotateZ(0.001);
+      this.rotateOnAxis(new Vector3(0, 1, 0).normalize(), 0.005);
       // tween.update();
     }
 
+    // tween stuff
 
 }
 
